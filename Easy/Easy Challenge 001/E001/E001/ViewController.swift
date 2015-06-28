3ff67642d7c6d86d9f63e68554ffa2721f21c8de
Patrick Bollinger <pjbollinger@student.ysu.edu>
2015-06-27 20:57:27 -0400

    E001 - Changed TextLabel to TextView because it doesn't support multiline out-of-the-box. Finished challenge!

--- Easy/Easy Challenge 001/E001/E001/ViewController.swift
+++ Easy/Easy Challenge 001/E001/E001/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var helloButton: UIButton!
+    
+    @IBOutlet weak var nameField: UITextField!
 
+    @IBOutlet weak var ageField: UITextField!
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var responseText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +28,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func respond(sender: AnyObject) {
+        responseText.text = "Hello \(nameField.text),\nI know you are \(ageField.text) year(s) old\nand go by \(usernameField.text) on Reddit!"
     }
 
 
